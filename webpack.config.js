fce80b8908531203644d22429a49c532984e4f53
Renz Vital <vital.renzjohn@gmail.com>
2020-12-07 22:14:59 -0800

    Added some styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const path = require("path");
 
+const postCSSPlugins = [
+    require("postcss-import"),
+    require("postcss-simple-vars"),
+    require("postcss-nested"),
+    require("autoprefixer"),
+];
+
 module.exports = {
     entry: "./src/app.js",
     output: {
@@ -13,9 +20,20 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            }
-        ]
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: postCSSPlugins,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
     },
 
 }
