92e1e88c85b057bed4e0ddb8041624c88c845a20
Renz Vital <vital.renzjohn@gmail.com>
2021-01-02 10:01:15 +0800

    Completed the reveal on scroll behavior.

--- src/script_modules/RevealOnScroll.js
+++ src/script_modules/RevealOnScroll.js
@@ -1,21 +1,26 @@
 import throttle from "lodash/throttle";
+import debounce from "lodash/debounce";
 
 class RevealOnScroll {
     constructor(elements, threshold) {
         this.thresholdPercent = threshold;
         this.itemsToReveal = elements;
+        this.windowHeight = window.innerHeight;
         this.hideInitially();
         this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
-        console.log(this);
         this.events();
     }
 
     events() {
         window.addEventListener("scroll", this.scrollThrottle);
+        window.addEventListener("resize", debounce(() => {
+            console.log("debounce ran -----------------------------");
+            this.windowHeight = window.innerHeight;
+        }, 333));
     }
 
     calcCaller() {
-        console.log("calcCaller is running....");
+        // console.log("calcCaller is running....");
         this.itemsToReveal.forEach(el => {
             if (!el.isRevealed) {
                 this.calculateIfScrolledTo(el);
@@ -24,18 +29,23 @@ class RevealOnScroll {
     }
 
     calculateIfScrolledTo(el) {
-        console.log("calculateIfScrolledTo is running ..");
-        let scrollPercent = (el.getBoundingClientRect().y / window.innerHeight) * 100;
-        if (scrollPercent < this.thresholdPercent) {
-            el.classList.add("reveal-item--is-visible")
-            el.isRevealed = true;
+        // console.log(window.scrollY + this.windowHeight, el.offsetTop);
+        // console.log("calculateIfScrolledTo is running ..");
 
-            // if its the current element
-            if (el.isLastItem) {
-                console.log(el);
-                window.removeEventListener("scroll", this.scrollThrottle);
+        if ((window.scrollY + this.windowHeight) > el.offsetTop) {
+            let scrollPercent = (el.getBoundingClientRect().y / this.windowHeight) * 100;
+            if (scrollPercent < this.thresholdPercent) {
+                el.classList.add("reveal-item--is-visible")
+                el.isRevealed = true;
+
+                // if its the current element
+                if (el.isLastItem) {
+                    window.removeEventListener("scroll", this.scrollThrottle);
+                }
             }
         }
+
+
     }
 
     hideInitially() {
